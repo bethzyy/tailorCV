84cc51e5c1e1d947f51f293ad28682885cf92911
bethz <bethz@263.net>
2026-04-11 22:45:01 +0800

    auto-fix: checkpoint before batch 25

--- core/model_manager.py
+++ core/model_manager.py
@@ -14,7 +14,6 @@ from .config import config
 
 logger = logging.getLogger(__name__)
 
-
 class ModelManager:
     """单模型管理器 - 使用提供者模式"""
 
@@ -83,8 +82,8 @@ class ModelManager:
 
         # 模型列表：首选 -> 备选
         models_to_try = [preferred_model] + self.fallback_models
-        # 去重并创建新列表副本
-        models_to_try = list(dict.fromkeys(models_to_try))[:]
+        # 使用列表切片创建副本
+        models_to_try = models_to_try[:]
 
         last_error = None
 
